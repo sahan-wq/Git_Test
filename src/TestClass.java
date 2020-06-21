12f4a65c158d9e17964861ae67c557fb8be6a9da
sahan-wq <67201333+sahan-wq@users.noreply.github.com>
2020-06-21 02:09:41 +0200

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,5 +1,7 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git den merhaba");
+        System.out.println("GitHup dan merhaba");
+        
     }
 }
